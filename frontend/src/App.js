21abd3a9e1673a00df8a70e54b575cb332606e82
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-03 20:45:01 +0200

    Header component has been implemented and integrated into the App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
-
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 const App = () => {
-  return <h1>Welcome To ProShop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
