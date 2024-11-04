5575ad9a15cddf55a9914f860908cf91a2128e71
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-04 10:09:05 +0200

    First server implemention

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("server has been initalized");
+import express from "express";
+const port = 5000; //port 3000 is used by the frontend
+
+const app = express();
+
+//GET REQUESTS
+app.get("/", (req, res) => {
+  res.send("API is running ...");
+});
+
+//Initalizing the server
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
