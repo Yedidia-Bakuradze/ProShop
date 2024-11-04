b063eed76aa2cddd4f52228e0f02d43bb0698a07
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-04 10:15:58 +0200

    GET API endpoint for retriving all the products has been served

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import products from "./data/products.js";
 const port = 5000; //port 3000 is used by the frontend
 
 const app = express();
@@ -8,5 +9,8 @@ app.get("/", (req, res) => {
   res.send("API is running ...");
 });
 
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 //Initalizing the server
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
