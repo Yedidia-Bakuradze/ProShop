8fb5da5c5ef9dc41e60473bfdae6ba26d208f0b9
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-04 10:18:15 +0200

    GET API endpoint for retriving a product based on it's id has been served

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,10 @@ app.get("/", (req, res) => {
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((pro) => pro._id === req.params.id);
+  res.json(product);
+});
 //Initalizing the server
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
