a44a17475e47d50736419e758d6e8322a4bc0798
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-19 22:36:50 +0200

    The product router for product URL requests has been added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000; //port 3000 is used by the frontend
 connectDB();
 const app = express();
@@ -11,14 +11,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is running ...");
 });
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((pro) => pro._id === req.params.id);
-  res.json(product);
-});
 //Initalizing the server
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
