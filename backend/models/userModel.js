ed913721bd4702a37231d5693aebc7ca968b1ce5
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 22:59:45 +0200

    Basic auth in user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,28 +1,32 @@
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+            required: true
+        }
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
 export default User;
