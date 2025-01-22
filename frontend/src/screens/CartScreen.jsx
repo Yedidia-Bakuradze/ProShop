85d116152e2d8a5b90459e91fb487fc8596518bd
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 19:33:26 +0200

    Endpoint to the cart screen has been added

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,21 @@
+import { Link, useNavigate } from 'react-router-dom';
+import {
+    Row,
+    Col,
+    ListGroup,
+    Image,
+    Form,
+    Button,
+    Card
+} from 'react-bootstrap';
+import { FaTrash } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+
+const CartScreen = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    return <div>CartScreen</div>;
+};
+
+export default CartScreen;
