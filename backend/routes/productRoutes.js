750b5464b3d3036b91c564c8cc420f22ada0b473
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 11:01:08 +0200

    Switched to new error handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -18,7 +18,8 @@ router.get(
             console.log(product);
             res.json(product);
         }
-        res.status(404).json({ message: 'Product not found' });
+        res.status(404);
+        throw new Error('Resource not found');
     })
 );
 
