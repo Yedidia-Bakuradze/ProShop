6e7b3c63daa8b79cccd1e6d0c19f7934c9bdc601
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 19:27:07 +0200

    Header has been updated to show number of items in the cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,40 +1,54 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { LinkContainer } from "react-router-bootstrap";
-import logo from "../assets/logo.png";
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
+import logo from '../assets/logo.png';
 const Header = () => {
-  return (
-    <header>
-      {/* The expand and  */}
-      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-        <Container>
-          {/* Barnd - The company title to navigate to the home site*/}
-          <LinkContainer to="/">
-            <Navbar.Brand>
-              <img src={logo} alt="ProShop" />
-              ProShop
-            </Navbar.Brand>
-          </LinkContainer>
-          {/* Toggle - dropdown menu managed by the Collapse component */}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <FaShoppingCart /> Cart{" "}
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaUser /> Sign In
-                </Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
+    const { cartItems } = useSelector((state) => state.cart); //Access to the cart from store.js
+    return (
+        <header>
+            {/* The expand and  */}
+            <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+                <Container>
+                    {/* Barnd - The company title to navigate to the home site*/}
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            <img src={logo} alt="ProShop" />
+                            ProShop
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    {/* Toggle - dropdown menu managed by the Collapse component */}
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <FaShoppingCart /> Cart
+                                    {cartItems.length > 0 && (
+                                        <Badge
+                                            pill
+                                            bg="success"
+                                            style={{ marginLeft: '5px' }}
+                                        >
+                                            {cartItems.reduce(
+                                                (a, c) => a + c.qty,
+                                                0
+                                            )}
+                                        </Badge>
+                                    )}
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>
+                                    <FaUser /> Sign In
+                                </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
 };
 
 export default Header;
