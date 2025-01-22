a83ec61b7defd51ef9c31906fd69e2e7d171850d
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 10:59:48 +0200

    New error handler implemented for better debugging

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,27 @@
+//This is the error manager file including the error handlers for the server
+
+//error when the object not found
+const noFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+};
+
+//Overirde the default express error handler
+const errorHandler = (err, req, res, next) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = res.message;
+
+    //Check for Mongoose bad ObjectId
+    if (err.name === 'CastError' && err.kind === 'ObjectId') {
+        message = 'Resource not found';
+        statusCode = 404;
+    }
+
+    res.status(statusCode).json({
+        message,
+        stack: process.env.NODE_ENV === 'production' ? '🖥️' : err.stack //Not overwhelme the console with debug error messages
+    });
+};
+
+export { noFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { noFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000; //port 3000 is used by the frontend
 connectDB();
 const app = express();
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
     res.send('API is running ...');
 });
 app.use('/api/products', productRoutes);
-
+app.use(noFound);
+app.use(errorHandler);
 //Initalizing the server
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
