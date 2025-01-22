ce3b32e40ca269f3a0ae05cba0ba292e79e2cc52
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 19:11:58 +0200

    Refactoring

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,15 +1,10 @@
 //Not api sicne with dont use async
 import { createSlice } from '@reduxjs/toolkit';
-
+import { updateCart } from '../untils/cartUtils';
 //Load existing data of cart, if not exsiting - initiate an empty array in cartItems
 const initialState = localStorage.getItem('cart')
     ? JSON.parse(localStorage.getItem('cart'))
     : { cartItems: [] };
-
-// Two decimal plcaes
-const addDecimals = (num) => {
-    return (Math.round(num * 100) / 100).toFixed(2);
-};
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -25,29 +20,7 @@ const cartSlice = createSlice({
             } else {
                 state.cartItems = [...state.cartItems, item];
             }
-
-            //Calculate the cart price
-            state.itemsPrice = addDecimals(
-                state.cartItems.reduce(
-                    (acc, item) => acc + item.price * item.qty,
-                    0
-                )
-            );
-            //Calculate the shipping price
-            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-            //Calculate the tax price
-            state.taxPrice = addDecimals(
-                Number((0.15 * state.itemsPrice).toFixed(2))
-            );
-            //Calculate the total price
-            state.totalPrice = (
-                Number(state.itemsPrice) +
-                Number(state.shippingPrice) +
-                Number(state.taxPrice)
-            ).toFixed(2);
-
-            //Save in local storge
-            localStorage.setItem('cart', JSON.stringify(state));
+            return updateCart(state);
         }
     } //The action that would be made and effect the state
 });
