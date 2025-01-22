888ae09e7466f0b819a3b768adf1ff51104a59a1
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 22:50:49 +0200

    Basic User controller & route has been added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,78 @@
+//Handels the logics
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js';
+
+// @desc	Auth user & get connetion token
+// @route	POST /api/users/login
+// @access	Public
+const authUser = asyncHandler(async (req, res) => {
+    res.send('auth user');
+});
+
+// @desc	Register user
+// @route	POST /api/users
+// @access	Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.send('register user');
+});
+
+// @desc	Logout user / clear cookie
+// @route	POST /api/users/logout
+// @access	Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.send('logout user');
+});
+
+// @desc	Get user profile
+// @route	GET /api/users/profile
+// @access	Public
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.send('user profile ');
+});
+
+// @desc	Update user
+// @route	PUT /api/users/profile
+// @access	Privde
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.send('update user');
+});
+
+// @desc	Get all users
+// @route	GET /api/users
+// @access	Private/admin
+const getUsers = asyncHandler(async (req, res) => {
+    res.send('all users');
+});
+
+// @desc	Get users by ID
+// @route	GET /api/users/:id
+// @access	Private/admin
+const getUserById = asyncHandler(async (req, res) => {
+    res.send('get user by ID');
+});
+
+// @desc	Delete user
+// @route	DELETE /api/users/:id
+// @access	Private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+    res.send('delete user');
+});
+
+// @desc	Update user
+// @route	PUT /api/users/:id
+// @access	Private/admin
+const updateUser = asyncHandler(async (req, res) => {
+    res.send('update users');
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser
+};
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
 import { noFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000; //port 3000 is used by the frontend
 connectDB();
@@ -13,6 +16,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
 app.use(noFound);
 app.use(errorHandler);
 
