7ba3d8ae69d9a1dee60f134aaabcbaab0ecb8f27
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 11:24:40 +0200

    redux's initialization

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,34 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider
+} from 'react-router-dom';
 
 // import "bootstrap/dist/css/bootstrap.min.css"; // Basic css file included in the boostrap installtion
-import "./assets/styles/bootstrap.custom.css";
-import "./assets/styles/index.css";
-import App from "./App";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import App from './App';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { Provider } from 'react-redux';
+import store from './store';
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />} />
-      <Route path="/product/:id" element={<ProductScreen />} />
-    </Route>
-  )
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route index={true} path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+        </Route>
+    )
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/store.js
@@ -0,0 +1,9 @@
+// This file is the state manager for the entier application
+
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: {}
+});
+
+export default store;
