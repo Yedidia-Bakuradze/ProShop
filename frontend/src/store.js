eb09eecae70717b8daf76135a43de9dc1ae96db4
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 11:34:56 +0200

    Main slice configured in the store.js file

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,15 @@
 // This file is the state manager for the entier application
 
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true
 });
 
 export default store;
