da6ade22abdf06f38e109852b0351f87316ecfd1
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-22 11:14:38 +0200

    Controllers been added to the backend for cleaner code

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,26 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
-router.get(
-    '/',
-    asyncHandler(async (req, res) => {
-        const products = await Product.find({});
-        res.json(products);
-    })
-);
+import {
+    getProductById,
+    getProducts
+} from '../controllers/productController.js';
+router.route('/').get(getProducts);
 
-router.get(
-    '/:id',
-    asyncHandler(async (req, res) => {
-        const product = await Product.findById(req.params.id);
-        if (product) {
-            console.log(product);
-            res.json(product);
-        }
-        res.status(404);
-        throw new Error('Resource not found');
-    })
-);
+router.route('/:id').get(getProductById);
 
 export default router;
