ceb90ff89597fa4b07fd67b21b5b5a5c397b8bce
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-27 10:20:12 +0200

    toastify injected into the site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import { Container } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Outlet } from 'react-router-dom';
 const App = () => {
-  return (
-    <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          {/* <h1>Welcome To ProShop</h1> */}
-          <Outlet />
-        </Container>
-      </main>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    {/* <h1>Welcome To ProShop</h1> */}
+                    <Outlet />
+                </Container>
+            </main>
+            <Footer />
+            <ToastContainer />
+        </>
+    );
 };
 
 export default App;
