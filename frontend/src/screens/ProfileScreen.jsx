8d3a5fb5d35ddc854d4e1d8739d03f94cebde4c9
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-02-26 18:18:01 +0200

    order table has been added, Order model has been fixed with createdAt field

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -2,12 +2,13 @@ import { useState, useEffect, act } from 'react';
 import { Button, Table, Form, Row, Col } from 'react-bootstrap';
 import { setCredentials } from '../slices/authSlice';
 import { LinkContainer } from 'react-router-bootstrap';
+import { FaTimes } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import { toast } from 'react-toastify';
 import Loader from '../components/Loader';
 import { useProfileMutation } from '../slices/usersApiSlice';
-
+import { useGetUserOrdersQuery } from '../slices/ordersApiSlice';
 const ProfileScreen = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -20,12 +21,14 @@ const ProfileScreen = () => {
     const [updateProfile, { isLoading: loadingProfileUpdate }] =
         useProfileMutation();
 
+    const { data: orders, isLoading, error } = useGetUserOrdersQuery();
     useEffect(() => {
+        console.log(orders);
         if (userInfo) {
             setName(userInfo.name);
             setEmail(userInfo.email);
         }
-    }, [userInfo, userInfo.name, userInfo.password]);
+    }, [orders, userInfo, userInfo.name, userInfo.password]);
 
     const submitHandler = async (e) => {
         e.preventDefault();
@@ -52,6 +55,7 @@ const ProfileScreen = () => {
 
     return (
         <Row>
+            {/* Profile details */}
             <Col md={3}>
                 <Form onSubmit={submitHandler}>
                     {/* Name field */}
@@ -113,7 +117,54 @@ const ProfileScreen = () => {
                     </Button>
                 </Form>
             </Col>
-            <Col md={9}></Col>
+
+            {/* User orders */}
+            <Col md={9}>
+                <h2>My Orders</h2>
+                {isLoading ? (
+                    <Loader />
+                ) : error ? (
+                    <Message variant="danger">
+                        {error?.error?.message || error.error}
+                    </Message>
+                ) : (
+                    <Table striped hover responsive className="table-sm">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Date</th>
+                                <th>Total</th>
+                                <th>Paid</th>
+                                <th>Delivered</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {orders.map((order) => (
+                                <tr key={order._id}>
+                                    <td>{order._id}</td>
+                                    <td>{order.createdAt}</td>
+                                    <td>${order.totalPrice}</td>
+                                    <td>
+                                        {order.isPaid ? (
+                                            order.paidAt.substring(0, 10)
+                                        ) : (
+                                            <FaTimes style={{ color: 'red' }} />
+                                        )}
+                                    </td>
+                                    <td>
+                                        {order.isDelivered ? (
+                                            order.deliveredAt
+                                        ) : (
+                                            <FaTimes style={{ color: 'red' }} />
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                )}
+            </Col>
         </Row>
     );
 };
