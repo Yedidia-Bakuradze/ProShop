caf421fa731750e399af5192a2fa4bbf214adafa
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-02-27 13:37:40 +0200

    url search feature was added

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,8 +6,11 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Paginate from '../components/Paginate';
 const HomeScreen = () => {
-    const { pageNumber } = useParams();
-    const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+    const { pageNumber, keyword } = useParams();
+    const { data, isLoading, error } = useGetProductsQuery({
+        keyword,
+        pageNumber
+    });
 
     return (
         <>
@@ -28,7 +31,11 @@ const HomeScreen = () => {
                         ))}
                     </Row>
 
-                    <Paginate page={data.page} pages={data.pages} />
+                    <Paginate
+                        page={data.page}
+                        pages={data.pages}
+                        keyword={keyword ? keyword : ''}
+                    />
                 </>
             )}
         </>
